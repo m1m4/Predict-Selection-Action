5aaf8af39b72f170554a5c040e148bfb4beec7b3
m1m4 <noamkadim@gmail.com>
2024-08-27 14:19:26 +0300

    added todo

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+//TODO: use vite for background.ts instead of rollup
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
